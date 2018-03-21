3d1c5bd0d5b544c76b9241bda6d203ef5ce17d22
Yuda Cogati <yudapc@gmail.com>
2018-03-21 13:22:58 +0700

    implement es6

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var request = require('request'),
+const request = require('request'),
     cheerio = require('cheerio');
 
-var urlTarget = 'https://weather.com/weather/today/l/IDXX0022:1:ID';
+const urlTarget = 'https://weather.com/weather/today/l/IDXX0022:1:ID';
 
-function main() {
-  return request(urlTarget, function (err, res, body) {
+const main = () => {
+  return request(urlTarget, (err, res, body) => {
     if (!err && res.statusCode === 200) {
-      var $ = cheerio.load(body);
-      var nowCardLocation = $('.today_nowcard h1.h4.today_nowcard-location').text();
-      var condition = $('.today_nowcard div.today_nowcard-phrase').text();
-      var temperatureF = $('.today_nowcard div.today_nowcard-temp span').text();
-      var convertTemperature = (parseFloat(temperatureF) - 32) * 5 / 9;
-      var temperatureC = Math.round(convertTemperature);
+      const $ = cheerio.load(body);
+      const nowCardLocation = $('.today_nowcard h1.h4.today_nowcard-location').text();
+      const condition = $('.today_nowcard div.today_nowcard-phrase').text();
+      const temperatureF = $('.today_nowcard div.today_nowcard-temp span').text();
+      const convertTemperature = (parseFloat(temperatureF) - 32) * 5 / 9;
+      const temperatureC = Math.round(convertTemperature);
       console.log('location: ', nowCardLocation);
       console.log('condition: ', condition);
       console.log('temperature: ', temperatureF + ' F --> ' + temperatureC + ' C');
